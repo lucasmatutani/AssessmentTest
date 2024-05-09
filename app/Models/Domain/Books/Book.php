a0aae9d84da7728bd6b610d90018c087eddced99
lucasmatutani <lucasmatutani@gmail.com>
2024-05-09 00:03:58 -0300

    feat: Creating migrations

---
+++ app/Models/Domain/Books/Book.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Domain\Books;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+}
