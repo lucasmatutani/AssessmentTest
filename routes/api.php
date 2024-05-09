03047f7538a815c9787501b1a9b044d5a725eed1
lucasmatutani <lucasmatutani@gmail.com>
2024-05-09 16:33:15 -0300

    feat: Creating user authentication

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Books\BookController;
 use App\Http\Controllers\Stores\StoreController;
+use App\Http\Controllers\Auth\AuthController;
+
 
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('books', BookController::class);
